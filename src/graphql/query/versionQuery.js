a7627a4ee17a47374b8f6ee255dd92254d7b1fc8
Austin Haws <ahaws@utah.gov>
2021-01-02 19:57:45 -0700

    Remove useless logging

--- src/graphql/query/versionQuery.js
+++ src/graphql/query/versionQuery.js
@@ -1,4 +1,3 @@
 import packageJson from '../../../package';
 
-console.log({packageJson});
-export default () => console.log('fetching version', {packageeJsonVersision: packageJson.version}) || packageJson.version;
+export default () => packageJson.version;
