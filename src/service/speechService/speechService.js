0acdf92a79b201db759898623594af8c88efc0e2
Austin Haws <ahaws@utah.gov>
2021-01-04 13:52:48 -0700

    Add Goblin Speech

--- src/service/speechService/speechService.js
+++ src/service/speechService/speechService.js
@@ -1,12 +1,14 @@
 const speechDao = require("../../dao/speechDao/speechDao");
 const blackSpeechData = require('./data/blackSpeechData');
 const elfSpeechData = require("./data/elfSpeechData");
+const goblinSpeechData = require("./data/goblinSpeechData");
 
 module.exports = {
     loadSpeeches: () => {
         const speeches = [
             blackSpeechData,
-            elfSpeechData
+            elfSpeechData,
+            goblinSpeechData,
         ];
 
         speeches.forEach(speechDao.saveSpeechData);
